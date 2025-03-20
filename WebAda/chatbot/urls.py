83893115c19aa21d6e34a998b322ff8a0322dd7a
Breno Macedo <brenonsmacedo@gmail.com>
2025-03-20 10:51:08 -0400

    remoçãoWebAda

--- WebAda/chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import chatbot_interface, chat_view  # ⬅ Adicionado chat_view corretamente
-
-urlpatterns = [
-    path("", chatbot_interface, name="chatbot_interface"),  # Página web
-    path("api/chat/", chat_view, name="chatbot_api"),  # API do chatbot
-]
